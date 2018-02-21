a10e6d8dca925ee3b907d2ce77c958311b57c37c
unpairestgood <paulesingman@gmail.com>
2018-02-21 13:16:56 -0500

    base of file uplaod

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
-from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
+from flask import Flask, render_template, session, redirect, url_for, flash, request, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = '/tmp'
 
 app = Flask(__name__)
-manager = Manager(app)
-bootstrap = Bootstrap(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+ALLOWED_EXTENSIONS = set(['csv'])
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+    # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
 
+    return render_template('base.html')
 
-
-if __name__ == '__main__':
-    manager.run()
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
